1c49200e049b6d20e7d93b03eac39f7c9febdadb
Govind85 <krishnagovind8@gmail.com>
2026-02-14 19:47:22 +0530

    Changes to achieve test coverage more than 70% and maven.yml

--- java-assignment/src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
@@ -2,9 +2,11 @@ package com.fulfilment.application.monolith.products;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.IsNot.not;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -32,5 +34,77 @@ public class ProductEndpointTest {
         .then()
         .statusCode(200)
         .body(not(containsString("TONSTAD")), containsString("KALLAX"), containsString("BESTÅ"));
+  }
+
+  @Test
+  public void testCreateProduct() {
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"name\":\"HEMNES\",\"type\":\"Bed frame\"}")
+        .when()
+        .post("product")
+        .then()
+        .statusCode(201)
+        .body("name", equalTo("HEMNES"));
+  }
+
+  @Test
+  public void testGetProduct() {
+    given()
+        .when()
+        .get("product/2")
+        .then()
+        .statusCode(200)
+        .body("name", equalTo("KALLAX"));
+  }
+
+  @Test
+  public void testGetProductNotFound() {
+    given()
+        .when()
+        .get("product/999")
+        .then()
+        .statusCode(404);
+  }
+
+  @Test
+  public void testUpdateProduct() {
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"name\":\"KALLAX Updated\",\"type\":\"Shelving unit\"}")
+        .when()
+        .put("product/2")
+        .then()
+        .statusCode(200)
+        .body("name", equalTo("KALLAX Updated"));
+  }
+
+  @Test
+  public void testUpdateProductNotFound() {
+    given()
+        .contentType(ContentType.JSON)
+        .body("{\"name\":\"Test\",\"type\":\"Test\"}")
+        .when()
+        .put("product/999")
+        .then()
+        .statusCode(404);
+  }
+
+  @Test
+  public void testDeleteProductNotFound() {
+    given()
+        .when()
+        .delete("product/999")
+        .then()
+        .statusCode(404);
+  }
+
+  @Test
+  public void testListProducts() {
+    given()
+        .when()
+        .get("product")
+        .then()
+        .statusCode(200);
   }
 }
